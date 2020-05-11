ecc523c50aa9c63078151654d1c2079d6ac328f7
Victor Hermes <me.victorhermes@gmail.com>
2020-05-11 19:55:11 -0300

    gerenciador financeiro

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,12 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Balanço deve ser maior que a despesa');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
